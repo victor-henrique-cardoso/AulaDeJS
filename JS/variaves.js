a0784f349f5b0136b08c00688ba6531fe5a14b53
victor henrique <victorcardosoderreirahenrique@gmail.com>
2025-11-02 11:13:31 -0400

    first commit

---
+++ JS/variaves.js
@@ -0,0 +1,15 @@
+//introdução a variaves 
+
+// string
+var nome = prompt('Informe seu nome')
+
+//number
+var idade = prompt('informe sua idade')
+var salario = prompt("informe seu salario")
+
+
+//alert(nome) //abrir um dialog
+
+document.write('Nome: ' + nome + " sua idade: " + idade + " Salario: " + salario) //escrever valor na tela
+
+console.log(idade) 
